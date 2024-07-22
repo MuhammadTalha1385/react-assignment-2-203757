0c1a4dc73375b8da24c81514500bcf9e593f45a1
MuhammadTalha1385 <161243306+MuhammadTalha1385@users.noreply.github.com>
2024-07-22 15:56:00 +0500

    components

---
+++ Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <>
+      <nav className="navbar bg-primary" >
+        <div className="container-fluid">
+          <span className="text-white mx-auto">All Rights Reserved</span>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Footer;
---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <nav className="navbar bg-primary py-3">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1 text-white">Todo App</span>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header;
